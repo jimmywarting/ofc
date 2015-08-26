85818dd98341cfba9497a2acd4a5c2c91ea46716
Jimmy Karl Roland Wärting <jimmy@warting.se>
2015-08-26 17:18:01 +0200

    Update service-worker.js

--- cron-job/service-worker.js
+++ cron-job/service-worker.js
@@ -14,10 +14,36 @@ function showNotification(title, body, icon, data) {
 }
 
 
+var reqConfig = (function(){var a=new FormData;a.append("format","svg");a.append("file",function(){for(var a=atob("d09GMgABAAAAAADMAAoAAAAAAhAAAACGAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABlYALAokNgE4AiQDBgsGAAQgBSAHEBtpAciuCuymx+8MIwgrn4ywhx+96xA9vxab93cFRbvtOLiUDLFbknSbZ+uLTgMAitbLf+DIejCuq1Y3OJNMg0QTzGmByQcabCCbMeB0CWienwKS4HKRXLKEl+3EtoUPHmwBGLAxWLgBNkhNvQEQ4v8wfH864VuCn39x1XTHzNs4LO2HoGEg"),e=[],b=0;b<a.length;b+=512){for(var c=a.slice(b,b+512),f=Array(c.length),d=0;d<c.length;d++)f[d]=c.charCodeAt(d);c=new Uint8Array(f);
+e.push(c)}return new Blob(e)}());return{method:"POST",headers:{"X-Mashape-Key":"dFYPWXxpp3mshKD6Kimb4pVfvYLvp1YWcWfjsnErOY3HN8zs4a"},body:a}})();
 
+function checkStatus(response) {
+	if (response.status === 200 && headers.get("content-type") === 'application/octet-stream') {
+		return response
+	} else {
+		var error = new Error(response.statusText)
+		error.response = response
+		throw error
+	}
+}
+
+
+	
 setInterval(function(){
-    var title = 'What\'s the weather like in London?';
-    var message = 'sunny';
-    var icon = 'https://dummyimage.com/128x128/000/fff';
-    showNotification(title, message, icon, {});
+    
+  fetch('https://ofc.p.mashape.com/directConvert/', reqConfig)
+	  .then(checkStatus)
+	  .then(function(response){
+	    var title = 'Font converter success';
+      var message = '';
+      var icon = 'https://dummyimage.com/128x128/000/fff';
+      
+	    showNotification(title, message, icon, {});
+	  })
+	  .catch(function(error) {
+		  var title = 'Font converter failed';
+      var message = 'for some reason';
+      var icon = 'https://dummyimage.com/128x128/000/fff';
+      showNotification(title, message, icon, {});
+	  });
 }, 10000);
