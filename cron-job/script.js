fea7aa6e63779e6cb9484a335db379e7798602dd
Jimmy Karl Roland Wärting <jimmy@warting.se>
2015-08-26 13:13:54 +0200

    Update script.js

--- cron-job/script.js
+++ cron-job/script.js
@@ -1,105 +1,44 @@
-// Code goes here
+var app = angular.module('StarterApp', ['ngMaterial']);
 
-var isPushEnabled = false;
+app.controller('AppCtrl', ['$mdSidenav', function($mdSidenav) {
 
-/*
-// Once the service worker is registered set the initial state  
-function initialiseState() {
+	this.pushEnabeld = false;
+	this.pushChanged = function() {
+		if(this.pushEnabeld){
+			subscribe();
+		} else {
+			unsubscribe();
+		}
+	};
 
-  // Check if push messaging is supported  
-  if (!('PushManager' in window)) {  
-    console.warn('Push messaging isn\'t supported.');  
-    return;  
-  }
+	function subscribe(){}
 
-  // We need the service worker registration to check for a subscription  
-  navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
-    // Do we already have a push message subscription?  
-    serviceWorkerRegistration.pushManager.getSubscription()  
-      .then(function(subscription) {  
-        // Enable any UI which subscribes / unsubscribes from  
-        // push messages.  
-        var pushButton = document.querySelector('.js-push-button');  
-        pushButton.disabled = false;
+	function subscribe() {
+		// Let's check if the user is okay to get some notification
+		if (Notification.permission === "granted") {
+			// If it's okay let's create a cron job
+			navigator.serviceWorker.register('service-worker.js');
+		}
 
-        if (!subscription) {  
-          // We aren't subscribed to push, so set UI  
-          // to allow the user to enable push  
-          return;  
-        }
-        
-        // Keep your server in sync with the latest subscriptionId
-        sendSubscriptionToServer(subscription);
+		// Otherwise, we need to ask the user for permission
+		// Note, Chrome does not implement the permission static property
+		// So we have to check for NOT 'denied' instead of 'default'
+		else if (Notification.permission !== 'denied') {
+			Notification.requestPermission(function(permission) {
 
-        // Set your UI to show they have subscribed for  
-        // push messages  
-        pushButton.textContent = 'Disable Push Messages';  
-        isPushEnabled = true;  
-      })  
-      .catch(function(err) {  
-        console.warn('Error during getSubscription()', err);  
-      });  
-  });  
-}
-*/
+				// Whatever the user answers, we make sure we store the information
+				if (!('permission' in Notification)) {
+					Notification.permission = permission;
+				}
 
-function subscribe() {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-    return;
-  }
-  
-  if (!('showNotification' in ServiceWorkerRegistration.prototype)) {  
-    alert("This browser does not support desktop notification from service worker"); 
-    return;  
-  }
-  
-  // Check that service workers are supported, if so, progressively  
-  // enhance and add push messaging support, otherwise continue without it.  
-  if (!'serviceWorker' in navigator) {
-    alert("This browser does not support service Worker");
-    return;
-  }
+				// If it's okay let's create a cron job
+				if (permission === "granted") {
+					navigator.serviceWorker.register('service-worker.js');
+				}
+			});
+		}
 
-  // Let's check if the user is okay to get some notification
-  if (Notification.permission === "granted") {
-    // If it's okay let's create a cron job
-    navigator.serviceWorker.register('service-worker.js');
-    alert("service worker registerd!");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  // Note, Chrome does not implement the permission static property
-  // So we have to check for NOT 'denied' instead of 'default'
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-
-      // Whatever the user answers, we make sure we store the information
-      if(!('permission' in Notification)) {
-        Notification.permission = permission;
-      }
-
-      // If it's okay let's create a cron job
-      if (permission === "granted") {
-        navigator.serviceWorker.register('service-worker.js');
-        alert("service worker registerd!");
-      }
-    });
-  }
-
-  // At last, if the user already denied any notification, and you 
-  // want to be respectful there is no need to bother him any more.
-}
-
-
-window.addEventListener('load', function() {  
-  var pushButton = document.querySelector('.js-push-button');  
-  pushButton.addEventListener('click', function() {  
-    if (isPushEnabled) {  
-      unsubscribe();  
-    } else {  
-      subscribe();  
-    }  
-  });
-});
+		// At last, if the user already denied any notification, and you 
+		// want to be respectful there is no need to bother him any more.
+	}
+}]);
