f0162d35e7c569ee2068c040b0a57159d645f485
Jimmy Karl Roland Wärting <jimmy@warting.se>
2015-08-26 15:12:46 +0200

    Update service-worker.js

--- cron-job/service-worker.js
+++ cron-job/service-worker.js
@@ -1,5 +1,23 @@
-self.registration.showNotification("checking", {  
-  body: "body",
-  icon: "http://onlinefontconverter.com/img/favicon/favicon.ico",  
-  tag: "simple-push-demo-notification-tag"
-}) 
+function showNotification(title, body, icon, data) {
+  var notificationOptions = {
+    body: body,
+    icon: icon ? icon : 'images/touch/chrome-touch-icon-192x192.png',
+    tag: 'simple-push-demo-notification',
+    data: data
+  };
+  if (self.registration.showNotification) {
+    self.registration.showNotification(title, notificationOptions);
+    return;
+  } else {
+    new Notification(title, notificationOptions);
+  }
+}
+
+
+
+setInterval(function(){
+    var title = 'What\'s the weather like in London?';
+    var message = 'sunny';
+    var icon = 'https://dummyimage.com/128x128/000/fff';
+    showNotification(title, message, icon, {});
+}, 10000);
